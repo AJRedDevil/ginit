2adb3638bffd64a6ff7430ec646e35fc8524358c
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-03-11 09:33:18 +0545

    Initializing the Node CLI

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const clear = require('clear');
 const figlet = require('figlet');
 
 const files = require('./lib/files');
+
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('Ginit', {horizontalLayout: 'full'})
+  )
+)
+
+if (files.directoryExists('.git')) {
+  console.log(chalk.red('Already a git repository!'));
+  process.exit();
+}
