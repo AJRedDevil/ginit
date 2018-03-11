7055a20d228952f8656405fcbe187bbe262df617
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-03-11 10:11:48 +0545

    Communicating with the GitHub API

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const figlet = require('figlet');
 // our packages
 const files = require('./lib/files');
 const inquirer = require('./lib/inquirer');
+const github = require('./lib/github');
 
 clear();
 console.log(chalk.yellow(figlet.textSync('Ginit', {horizontalLayout: 'full'})));
@@ -18,8 +19,12 @@ if (files.directoryExists('.git')) {
 */
 
 const run = async () => {
-  const credentials = await inquirer.askGithubCredentials();
-  console.log(credentials);
+  let token = github.getStoredGithubToken();
+  if(!token) {
+    await github.setGithubCredentials();
+    token = await github.registerNewToken();
+    console.log(token);
+  }
 };
 
 run();
